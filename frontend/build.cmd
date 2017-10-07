a05d1e6c658c223d3661bec74efabb1d5fded4b8
Michael.Ilyin <milyin@gmail.com>
2017-10-07 23:11:02 +0300

    elm custom step updated

--- frontend/build.cmd
+++ frontend/build.cmd
@@ -1,2 +1,5 @@
-elm-make --output=../static/elm.js src/zhaba.elm
+for %%i in (%0) do set THIS=%%~dpi
+mkdir %THIS%..\static
+copy %THIS%index.html %THIS%..\static
+elm-make --output=%THIS%..\static\elm.js %THIS%src\zhaba.elm --yes
 
